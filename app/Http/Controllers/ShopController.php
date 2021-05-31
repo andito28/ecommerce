a3ffc576846abecadebb1a432bc381d7618520f0
andito28 <andito763@gmail.com>
2021-05-31 10:12:00 +0800

    update shopcontroller

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -18,16 +18,16 @@ class ShopController extends Controller
 
     public function index(request $request,$id = null)
     {
-        // $categoris = DB::table('categoris')->simplePaginate(8);
-        $categoris = Categori::orderByRaw('RAND()')->take(9)->get();
+        // $categoris = DB::table('categoris')->simplePaginate(9);
+        $categoris = Categori::orderBy('created_at','ASC')->take(9)->get();
         $products  = Product::where('name','LIKE','%'.$request->search.'%')->simplePaginate(6);
         return view('shop.index',compact('products','categoris','id'));
     }
 
     public function show($id)
     {
-        // $categoris = DB::table('categoris')->simplePaginate(8);
-        $categoris = Categori::orderByRaw('RAND()')->take(9)->get();
+        // $categoris = DB::table('categoris')->simplePaginate(9);
+        $categoris = Categori::orderBy('created_at','ASC')->take(9)->get();
         $product = Product::where('id',$id)->first();
         return view('shop.show',compact('product','categoris'));
     }
@@ -35,8 +35,8 @@ class ShopController extends Controller
     public function categori($id)
     {
         $products = Product::where('categori_id',$id)->simplePaginate(6);
-        $categoris = Categori::orderByRaw('RAND()')->take(9)->get();
-        // $categoris = DB::table('categoris')->simplePaginate(8);
+        $categoris = Categori::orderBy('created_at','ASC')->take(9)->get();
+        // $categoris = DB::table('categoris')->simplePaginate(9);
         return view('shop.index',compact('products','categoris','id'));
     }
 }
